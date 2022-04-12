e94e78678184f6284b3e96a0185ecf01ebf27c48
Jacob Gelman <jacobgelman@icloud.com>
2022-04-12 14:58:03 -0700

    Add todo

--- ProcessSimulatorBasic.ino
+++ ProcessSimulatorBasic.ino
@@ -4,6 +4,13 @@
 #include "setup.h"
 #include "Process.hpp"
 
+// TODO:
+// 1. Add reset function
+// 2. Test alt. GPIO module
+// 3. Bug fix: serial
+// 4. Bug fix: dont start sim. untill LEDs ready
+// 5. Add screen
+
 // Time between computing simulation steps
 const int STEP_DELAY = 500;
 
