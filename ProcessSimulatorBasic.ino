eeec63ed88ed8b0e1d3a23e624839a047783bcab
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2022-04-21 15:01:46 -0700

    Create global config file, move setup code into .ino

--- ProcessSimulatorBasic.ino
+++ ProcessSimulatorBasic.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <P1AM.h>
 
-#include "setup.h"
+#include "config.h"
 #include "Process.hpp"
 
 // TODO:
@@ -16,7 +16,31 @@ Process *process;
 
 void setup()
 {
-    performSetup();
+    Serial.begin(115200);
+
+    // Wait for modules to come online.
+    while (!P1.init()) {}
+
+    // Print current module configuration and firmware version.
+    P1.printModules();
+    P1.getFwVersion();
+
+    // Ensure modules are plugged in as expected.
+    uint8_t errorFlags = P1.rollCall(MODULE_CONFIG, MODULE_COUNT);
+    if (errorFlags != 0)
+    {
+        // Turn on the yellow LED.
+        pinMode(LED_BUILTIN, OUTPUT);
+        digitalWrite(LED_BUILTIN, HIGH);
+
+        // Print the given message every ten seconds until power is cycled.
+        while (true)
+        {
+            Serial.println("Module error");
+            delay(10000);
+        }
+    }
+
     process = new Process();
 }
 
--- config.h
+++ config.h
@@ -1,5 +1,3 @@
-#ifndef SETUP_H
-#define SETUP_H
 #pragma once
 
 // The number of modules being used.
@@ -10,8 +8,3 @@ static const char *MODULE_CONFIG[MODULE_COUNT] = {
     "P1-15TD1", // slot 1 (3.3-24VDC sink output)
     "P1-16ND3"  // slot 2 (12-24VDC input)
 };
-
-void performSetup();
-static void fatalError(const char *message);
-
-#endif
